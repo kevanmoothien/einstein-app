6388f7605ef9a7d6e9d5aef3af0150d4f3b6dd23
kevanmoothien <kevanmoothien@gmail.com>
2021-03-14 14:58:45 +0400

    model layout started

--- src/app/model/model.component.ts
+++ src/app/model/model.component.ts
@@ -9,15 +9,30 @@ import {ElectronService} from "../core/services";
 })
 export class ModelComponent implements OnInit {
   private id: string;
+  record: any;
+  model: { modelId:string, progress:number, status:string, name:string };
+  progress: number;
 
   constructor(
     private route: ActivatedRoute,
     private electronService: ElectronService,
     private zone: NgZone
   ) {
+    this.model = { modelId: undefined, progress: 0, status: 'UNKNOWN', name: 'Unknown' };
     this.zone.run(() => {
       this.id = this.route.snapshot.paramMap.get('id');
     });
+    this.electronService.ipcRenderer.on('modelLoaded', (event, data)=> {
+      if (data) {
+        this.zone.run(() => {
+          this.record = data;
+          console.log('************** ', this.record);
+          this.model = this.record.model;
+          this.progress = this.model.progress * 100;
+        });
+      }
+    });
+    this.electronService.ipcRenderer.send('loadModel', { project_id: this.id });
   }
 
   ngOnInit(): void {
--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ModelRoutingModule } from './model-routing.module';
 import { ModelComponent } from "./model.component";
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
   declarations: [ModelComponent],
   imports: [
     CommonModule,
-    ModelRoutingModule
+    ModelRoutingModule,
+    SharedModule
   ]
 })
 export class ModelModule { }
